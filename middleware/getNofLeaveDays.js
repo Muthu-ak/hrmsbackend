eb4ca4c2075ae7e9ceec01154761f0415eb13b35
Muthu-ak <muthuak457@gmail.com>
2025-01-11 01:07:18 +0530

    11/1

---
+++ middleware/getNofLeaveDays.js
@@ -0,0 +1,26 @@
+const db = require("../config/db");
+const moment = require("moment");
+
+const getNofLeaveDays = async(req, res, next) =>{
+    let {start_date, end_date, leave_id} = req.body;
+    
+    if(leave_id  == -1){
+        let x = moment(payroll_date).format("YYYY-MM-DD");
+        let sql = `SELECT pr.payroll_id FROM payroll pr WHERE pr.is_deleted = 0 AND pr.user_login_id = ${user_login_id} 
+        AND DATE_FORMAT(pr.payroll_month, "%m-%Y") = DATE_FORMAT(DATE('${x}'), '%m-%Y')`;
+   
+        let [rows] = await db.execute(sql);
+    
+        if(rows.length > 0){
+            return res.status(400).json({'msg':'Employee data already exists for this month'});
+        }
+        else{
+            next();
+        }
+    }
+    else{
+        next();
+    }
+}
+
+module.exports = getNofLeaveDays;
